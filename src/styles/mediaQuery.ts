f59d3a0ae56d290bee0315a9349ea6ae1c8f2d14
IwannabeRealnerD <iwannaberealnerd@deeplehr.com>
2022-06-14 23:13:06 +0900

    [Feat] globalStyles, mediaQuery 추가

---
+++ src/styles/mediaQuery.ts
@@ -0,0 +1,5 @@
+export const DESKTOP = `@media (max-width: 1256px)`;
+export const TABLET = `@media (max-width: 1024px)`;
+export const MOBILE = `@media (max-width: 768px)`;
+export const PC_HOVER = `@media (hover:hover)`;
+export const M_HOVER = `@media (hover:none)`;
